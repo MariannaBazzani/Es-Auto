22885507c69ad29fa62aef3d97092eadc603e37c
MariannaBazzani <merionbaz@gmail.com>
2024-01-23 17:02:27 +0100

    Aggiungere i file di progetto.

---
+++ Es Auto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Auto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto a1= new Auto();
+            a1.Modello = "Ferrari";
+            Console.WriteLine(a1.Modello);
+
+            Auto a2 = a1;
+            Console.WriteLine(a2.Modello);
+
+            Auto a3 = new Auto();
+            a1 = a3;
+            Console.WriteLine(a1.Modello);
+        }
+    }
+}
